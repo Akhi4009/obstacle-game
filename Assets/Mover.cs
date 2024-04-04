a8fdd8a28cb11c167768c2643f6a857e541a94d3
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-04-04 08:16:24 +0530

    add material to player and plane

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+
+    [SerializeField] float xValue = 0f;
+    [SerializeField] float yValue = 0.02f;
+    [SerializeField] float zValue = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Translate(xValue, yValue, zValue);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(xValue, yValue, zValue);
     }
 }
