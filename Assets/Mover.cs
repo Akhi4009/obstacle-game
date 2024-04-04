2d5a73ee78576d5a112b2c2d25a8edcf924fe2ed
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-04-04 21:32:02 +0530

    making some changes

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
 
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0.02f;
-    [SerializeField] float zValue = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        transform.Translate(xValue, yValue, zValue);
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        MovePlayer();
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("welcome to the game");
+        Debug.Log("Move your player with WASD or arrow key");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0, zValue);
     }
 }
+
