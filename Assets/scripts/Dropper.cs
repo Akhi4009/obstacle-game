4fdc78c2f9b115cdc92fc7d120c2a70e47e95f71
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-04-05 10:10:58 +0530

    add drop functionality

--- Assets/scripts/Dropper.cs
+++ Assets/scripts/Dropper.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Drpper : MonoBehaviour
 {
+    MeshRenderer renderere;
+    Rigidbody rigidBody;
+    [SerializeField] float timeToWait = 5;
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Akhilesh yadav");
+        renderere = GetComponent<MeshRenderer>();
+        rigidBody = GetComponent<Rigidbody>();
 
+        renderere.enabled = false;
+        rigidBody.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > 3)
+        if (Time.time > timeToWait)
         {
-            Debug.Log("drop may be falling");
+            renderere.enabled = true;
+            rigidBody.useGravity = true;
         }
     }
 }
